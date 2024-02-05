e88589c3d00b8ac4ff32f1e69ec1d26411472289
ajithviswa <ajithviswanathan03@gmail.com>
2024-02-05 21:21:26 +0530

    Updated pom

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -1,13 +1,13 @@
 package launchBrowser;
 
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class firstTest {
   @Test
   public void f() {
-	  ChromeDriver driver = new ChromeDriver(); 
+	  WebDriver driver = new ChromeDriver(); 
 		driver.get("https://google.com");
 		System.out.println("Browser opened the url");
 		driver.close();
