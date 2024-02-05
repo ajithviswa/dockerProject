ae056cff5fbbfabcaa29c8a03b628cd2fbec729c
ajithviswa <ajithviswanathan03@gmail.com>
2024-02-05 20:09:55 +0530

    Fix

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -1,21 +1,13 @@
 package launchBrowser;
 
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class firstTest {
   @Test
-  @Parameters({"OS", "browser"})
-  public void f(String os, String br) {
-//	  ChromeDriver driver = new ChromeDriver();
-	  WebDriver driver = null;
-//	  switch(br.toLowerCase()) {
-//	  case "chrome": driver=new  ChromeDriver();
-//	  }
-	  driver=new  ChromeDriver();
-	 
+  public void f() {
+	  ChromeDriver driver = new ChromeDriver(); 
 		driver.get("https://google.com");
 		System.out.println("Browser opened the url");
 		driver.close();
