8735ac395b45eaab538e2758d5eabee129890a3f
Ajith <88364186+ajithviswa@users.noreply.github.com>
2024-02-06 19:48:53 +0530

    Update firstTest.java

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class firstTest {
   @Test
   public void f() {
-	 WebDriver driver = new ChromeDriver(); 
-	  ChromeOptions options = new ChromeOptions();
-	  options.addArguments("no-sandbox");
-	  options.addArguments("headless");
-	  options.addArguments("--disable-dev-shm-usage");
-	  driver = new ChromeDriver(options);
+	WebDriver driver = new FirefoxDriver(); 
+	 // WebDriver driver = new ChromeDriver(); 
+	  // ChromeOptions options = new ChromeOptions();
+	  // options.addArguments("no-sandbox");
+	  // options.addArguments("headless");
+	  // options.addArguments("--disable-dev-shm-usage");
+	  // driver = new ChromeDriver(options);
 		driver.get("https://google.com/");
 		System.out.println("Browser opened the url");
 		System.out.println("Page title is : " + driver.getTitle());
