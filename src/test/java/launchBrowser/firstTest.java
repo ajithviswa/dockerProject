78e026fc6408ff3ae68f7fd8dff67b4d42ca0904
Ajith <88364186+ajithviswa@users.noreply.github.com>
2024-02-06 19:49:19 +0530

    Update firstTest.java

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -2,6 +2,7 @@ package launchBrowser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
