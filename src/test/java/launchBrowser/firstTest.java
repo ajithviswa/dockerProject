90e44001c0504284eef834296c89bc67c43accd8
Ajith <88364186+ajithviswa@users.noreply.github.com>
2024-02-06 19:05:14 +0530

    Update firstTest.java

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -3,14 +3,18 @@ package launchBrowser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class firstTest {
   @Test
   public void f() {
-	  WebDriver driver = new ChromeDriver(); 
-		driver.get("https://mvnrepository.com/");
+	 WebDriver driver = new ChromeDriver(); 
+	  ChromeOptions options = new ChromeOptions();
+	  options.addArguments("no-sandbox");
+	  driver = new ChromeDriver(options);
+		driver.get("https://google.com/");
 		System.out.println("Browser opened the url");
-	  System.out.println("Page title is : " + driver.getTitle());
+		System.out.println("Page title is : " + driver.getTitle());
 		driver.close();
 		driver.quit();
   }
