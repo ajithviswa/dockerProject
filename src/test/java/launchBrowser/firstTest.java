8896a5714e956c1951450727c2f85f945a1d194a
Ajith <88364186+ajithviswa@users.noreply.github.com>
2024-02-18 13:29:28 +0530

    Update firstTest.java

--- src/test/java/launchBrowser/firstTest.java
+++ src/test/java/launchBrowser/firstTest.java
@@ -1,16 +1,27 @@
 package launchBrowser;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+@Test
 public class firstTest {
-  @Test
-  public void f() {
-	// WebDriver driver = new FirefoxDriver(); 
-	  WebDriver driver = new ChromeDriver(); 
+  public void f() throws MalformedURLException {
+	  
+	  ChromeOptions options = new ChromeOptions();
+	  DesiredCapabilities capabilities = new DesiredCapabilities();
+	  capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+	  URL url= new URL("http://localhost:4444"); 
+	  RemoteWebDriver driver = new RemoteWebDriver(url,capabilities) ;
+//	WebDriver driver = new FirefoxDriver(); 
+//	  WebDriver driver = new ChromeDriver(); 
 	  // ChromeOptions options = new ChromeOptions();
 	  // options.addArguments("no-sandbox");
 	  // options.addArguments("headless");
@@ -23,3 +34,4 @@ public class firstTest {
 		driver.quit();
   }
 }
+
